ec9403ac0203ccdc6d10a001b37ac71ee3ed887e
Chris <chris@canary.is>
2014-11-19 18:39:13 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Integrating Sphere
+This Arduino Project controls the RGB LED for an integrating sphere we 3D printed.  You can read
+more on how we use this device on our [blog](http://blog.canary.is/2014/11/20/a-custom-canary-integrating-sphere).
+
+Check out the 3D Model on [Thingverse](http://www.thingiverse.com/thing:554161) and the circuit on [Upverter](https://upverter.com/Canary/e8dc7b97dd5d9624/Integrating-Sphere/).
---
+++ sphere/sphere.ino
@@ -0,0 +1,36 @@
+// LED program 
+
+// Output
+int redPin = 9;   // Red LED,   connected to digital pin 9
+int grnPin = 10;  // Green LED, connected to digital pin 10
+int bluPin = 11;  // Blue LED,  connected to digital pin 11
+
+
+
+
+void setup()
+{
+  pinMode(redPin, OUTPUT);   // sets the pins as output
+  pinMode(grnPin, OUTPUT);   
+  pinMode(bluPin, OUTPUT); 
+}
+
+int redVal=255;
+int grnVal=255;
+int bluVal=255;
+
+void loop()
+{
+    
+ 
+   redVal = random(255);
+   grnVal = random(255);
+   bluVal = random(255);
+
+    analogWrite(redPin, redVal);      
+    analogWrite(grnPin, grnVal);      
+    analogWrite(bluPin, bluVal);      
+
+    delay(300);
+
+}
